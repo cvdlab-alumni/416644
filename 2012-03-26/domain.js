6556d9e9a48a804c6dc8e3bd685af2ede14c512c
Alessio Conte <ale.conte89@gmail.com>
2012-03-26 16:40:49 +0200

    lol

---
+++ 2012-03-26/domain.js
@@ -0,0 +1,15 @@
+
+// author: Alessio Conte
+
+
+
+var d1 = DOMAIN([[2,6]])([4]);
+DRAW(d1);
+
+
+var d2 = DOMAIN([[2,6],[1,4]])([4,3]);
+DRAW(d2);
+
+
+var d3 = DOMAIN([[2,6],[1,4],[3,5]])([4,3,2]);
+DRAW(d3);
