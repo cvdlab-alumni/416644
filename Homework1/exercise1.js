b8ef363579f967307535232b6ed8b1fcb114462d
Alessio Conte <ale.conte89@gmail.com>
2012-04-04 11:13:33 +0200

    ex1

--- Homework1/exercise1.js
+++ Homework1/exercise1.js
@@ -8,11 +8,11 @@ var lineeVerticali = function(){
 	lines.push(POLYLINE([[0,0],[0,2]]));
 	lines.push(POLYLINE([[1,0],[1,22]]));
 
-	for (i=2; i<9 ; i++){ //piscina-bagno
+	for (i=2; i<10 ; i++){ //piscina-bagno
 		lines.push(POLYLINE([[i,0],[i,1]]));
 		lines.push(POLYLINE([[i,10],[i,22]]));
 	}
-	for (i=9; i<21; i++){ //fine piscina
+	for (i=10; i<21; i++){ //fine piscina
 		lines.push(POLYLINE([[i,0],[i,1]]));
 		lines.push(POLYLINE([[i,10],[i,17]]));
 	}
@@ -42,11 +42,11 @@ var lineeOrizzontali = function(){
 	for (i in [0,1,2]){
 		lines.push(POLYLINE([[0,i],[1,i]]));
 	}
-	//linea 1-8
+	//linea 1-9
 	array = [17,18,19,20,21,22];
 	for (i in array){
 		console.log(array[i]);
-		lines.push(POLYLINE([[1,array[i]],[8,array[i]] ] ));
+		lines.push(POLYLINE([[1,array[i]],[9,array[i]] ] ));
 	}
 	//linea 1-21
 	array = [0,1,10,11,12,13,14,15,16,17];
@@ -84,12 +84,29 @@ var lineeOrizzontali = function(){
 }
 var griglia = STRUCT([lineeOrizzontali(),lineeVerticali()]);
 
-var mkRect = function(x0,y0,x1,y1){
-	return	POLYLINE([[x0,y0],[x0,y1],[x1,y1],[x1,y0],[x0,y0]]);
+var mkRect = function(x0,y0,w,h){
+	return POLYLINE([[x0,y0],[x0,y0+h],[x0+w,y0+h],[x0+w,y0],[x0,y0]]);
 }
 
 var mkMuretti = function(){
-	return mkRect(3,3,9,8);
+	var muri = [];
+	muri.push(mkRect(0.8,0.8,0.2,21.6));//piscina vert
+	muri.push(mkRect(0.8,0.8,7.2,0.2)); //piscina sotto
+
+	muri.push(mkRect(0.8,22.2,8.4,0.2));//ufficio sopra
+	muri.push(mkRect(9,16.8,0.2,1.8));  //ufficio destra sotto
+	muri.push(mkRect(9,16.8+1.8,0.2,1.8));  //ufficio destra centro
+	muri.push(mkRect(9,16.8+1.8*2,0.2,2));  //ufficio destra sopra
+	muri.push(mkRect(1,17,6,0.1));  //ufficio sotto sx
+	muri.push(mkRect(8,17,1,0.1));  //ufficio sotto dx
+	muri.push(mkRect(4.95,17,0.1,2.1));  //ufficio centro sotto
+	muri.push(mkRect(4.95,19.9,0.1,2.3));  //ufficio centro sopra
+	
+
+	muri.push(mkRect(7.5,15,19,0.2)); //muro sopra la piscina
+	
+
+	return STRUCT(muri);
 }
 var muretti = mkMuretti();
 
