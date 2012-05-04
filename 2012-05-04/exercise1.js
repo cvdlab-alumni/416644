1effbf6721df491da1711806b86996c3817d3cf8
Alessio Conte <ale.conte89@gmail.com>
2012-05-04 23:05:44 +0200

    ala prima parte smussata bug corretti

--- 2012-05-04/exercise1.js
+++ 2012-05-04/exercise1.js
@@ -26,10 +26,15 @@ var getBezierS1 = function (controls,n,draw){
 }
 
 
+var lunghezzaAla = 5;
+var lunghezzaCentroAla = 1;
+
 
 var domain = INTERVALS(1)(20);
 var scheletroAlaBase = [[1.5,0,0],[0.1,0.05,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0.3,0],[1.5,0,0]];
-var scheletroAlaFine = [[1.5,0,lungezzaAla],[1.3,0,lungezzaAla+0.5],[1,0,lungezzaAla+0.5],[0.4,0,lungezzaAla+0.5],[0.2,0,lungezzaAla+0.5],[0.1,0,lungezzaAla],[0,0,lungezzaAla],[0.1,0,lungezzaAla],[0.2,0,lungezzaAla+0.5],[0.4,0,lungezzaAla+0.5],[1.1,0,lungezzaAla+0.5],[1.3,0,lungezzaAla+0.5],[1.5,0,lungezzaAla]];
+var scheletroAlaFine = [[1.5,0,lunghezzaAla],[1.3,0,lunghezzaAla+0.5],[1,0,lunghezzaAla+0.5],[0.4,0,lunghezzaAla+0.5],[0.2,0,lunghezzaAla+0.5],
+						[0.1,0,lunghezzaAla],[0,0,lunghezzaAla],[0.1,0,lunghezzaAla],[0.2,0,lunghezzaAla+0.5],[0.4,0,lunghezzaAla+0.5],[1.1,0,lunghezzaAla+0.5],
+						[1.3,0,lunghezzaAla+0.5],[1.5,0,lunghezzaAla]];
 var mapping = BEZIER(S0)(scheletroAlaBase);
 
 var c = MAP(mapping)(domain);
@@ -43,13 +48,13 @@ for(var i = 0; i<11; i++){
 	scheletri.push(
 		scheletroAlaBase.map(
 						function(el){
-							return [el[0],el[1],el[2]+((i*lungezzaAla)/10)];}
+							return [el[0],el[1],el[2]+((i*lunghezzaAla)/10)];}
 							)
 					);
 }
 scheletri.push(scheletroAlaFine);
 
-var dbzs = AA(getBezier)(scheletri);
+var dbzs = AA(getBezierS0)(scheletri);
 var bzs = AA(BEZIER(S0))(scheletri);
 
 
--- 2012-05-04/testala.js
+++ 2012-05-04/testala.js
@@ -1,14 +1,3 @@
-/*
-var domain = INTERVALS(1)(20);
-var bezpoints = [[10,0,0],[0,5,0],[0,-3,0],[5,2,0],[10,0,0]];
-var mapping = BEZIER(S0)(bezpoints);
-
-var c = MAP(mapping)(domain);
-DRAW(c);
-DRAW(COLOR([0.7,0.7,0.7])(POLYLINE(bezpoints)));
-*/
-
-
 
 var getBezierS0 = function (controls,n,draw){
 	
@@ -36,28 +25,16 @@ var getBezierS1 = function (controls,n,draw){
 	return c;
 }
 
-var getBezier = function (controls,n,draw){
-	
-	var domain = INTERVALS(1)(n || 20);
-	var mapping = BEZIER(S0)(controls);
 
-	var c = MAP(mapping)(domain);
-	if(draw){
-		DRAW(c);
-	}
-
-	return c;
-}
-
-var lungezzaAla = 5;
+var lunghezzaAla = 5;
 var lunghezzaCentroAla = 1;
-
-
 
 
 var domain = INTERVALS(1)(20);
 var scheletroAlaBase = [[1.5,0,0],[0.1,0.05,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0.3,0],[1.5,0,0]];
-var scheletroAlaFine = [[1.5,0,lungezzaAla],[1.3,0,lungezzaAla+0.5],[1,0,lungezzaAla+0.5],[0.4,0,lungezzaAla+0.5],[0.2,0,lungezzaAla+0.5],[0.1,0,lungezzaAla],[0,0,lungezzaAla],[0.1,0,lungezzaAla],[0.2,0,lungezzaAla+0.5],[0.4,0,lungezzaAla+0.5],[1.1,0,lungezzaAla+0.5],[1.3,0,lungezzaAla+0.5],[1.5,0,lungezzaAla]];
+var scheletroAlaFine = [[1.5,0,lunghezzaAla],[1.3,0,lunghezzaAla+0.5],[1,0,lunghezzaAla+0.5],[0.4,0,lunghezzaAla+0.5],[0.2,0,lunghezzaAla+0.5],
+						[0.1,0,lunghezzaAla],[0,0,lunghezzaAla],[0.1,0,lunghezzaAla],[0.2,0,lunghezzaAla+0.5],[0.4,0,lunghezzaAla+0.5],[1.1,0,lunghezzaAla+0.5],
+						[1.3,0,lunghezzaAla+0.5],[1.5,0,lunghezzaAla]];
 var mapping = BEZIER(S0)(scheletroAlaBase);
 
 var c = MAP(mapping)(domain);
@@ -71,13 +48,13 @@ for(var i = 0; i<11; i++){
 	scheletri.push(
 		scheletroAlaBase.map(
 						function(el){
-							return [el[0],el[1],el[2]+((i*lungezzaAla)/10)];}
+							return [el[0],el[1],el[2]+((i*lunghezzaAla)/10)];}
 							)
 					);
 }
 scheletri.push(scheletroAlaFine);
 
-var dbzs = AA(getBezier)(scheletri);
+var dbzs = AA(getBezierS0)(scheletri);
 var bzs = AA(BEZIER(S0))(scheletri);
 
 
